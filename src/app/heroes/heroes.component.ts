32f946d06f8f23034625c3419689b7d075af276d
tkim949 <tkim949@gmail.com>
2020-11-04 11:27:28 -0800

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+  //below is a little different on the website, which doesn't have void
+  ngOnInit(): void {
+  }
+
+}
